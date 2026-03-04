3766488519e8929de6460380dc5e326dd0671a98
eosborne05 <eosborne826@gmail.com>
2026-03-04 16:27:17 -0500

    Generate Sphinx documentation output

---
+++ docs/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+import django
+sys.path.insert(0, os.path.abspath('../..')) # Points to your Django project root
+os.environ['DJANGO_SETTINGS_MODULE'] = 'news_portal_config.settings'
+django.setup()
+
+project = 'News_Portal'
+copyright = '2026, Eli Osborne'
+author = 'Eli Osborne'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+.. News_Portal documentation master file, created by
+   sphinx-quickstart on Wed Mar  4 16:21:11 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+News_Portal documentation
+=========================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
