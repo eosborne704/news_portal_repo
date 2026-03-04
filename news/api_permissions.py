7c4edeeadfa6cf496c229ef179dd5eb6d734b665
eosborne05 <eosborne826@gmail.com>
2026-03-04 16:02:05 -0500

    Add docstrings to api_permissions.py

--- news/api_permissions.py
+++ news/api_permissions.py
@@ -1,18 +1,30 @@
 from rest_framework import permissions
 
 class IsJournalist(permissions.BasePermission):
+    """
+    Allow only journalist users.
+    """
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == 'journalist'
 
 class IsEditor(permissions.BasePermission):
+    """
+    Allow only editor users.
+    """
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == 'editor'
 
 class IsReader(permissions.BasePermission):
+    """
+    Allow only reader users.
+    """
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == 'reader'
 
 class IsOwnerOrEditor(permissions.BasePermission):
+    """
+    Allow owner or editor users.
+    """
     def has_object_permission(self, request, view, obj):
         # Editors or owner (journalist) can edit/delete
         if request.user.role == 'editor':
